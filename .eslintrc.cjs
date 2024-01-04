f8132e5e6124c1bf17356686bf9ee7837147c299
tangwancheng <tangwan.spare@gmail.com>
2024-01-04 15:43:42 +0800

    ⭐ feat：eslint 关闭vue组件名的检查

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
@@ -12,5 +12,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest'
   },
-  rules:{ 'vue/multi-word-component-names': 'off'}
+  rules: {
+    'vue/multi-word-component-names': 'off' // 关闭vue组件 必须用多单词的组件名的检查
+  }
 }
