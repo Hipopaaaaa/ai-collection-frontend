67398cc4413d80ca45f647c79ee4b924dad96f4d
tangwancheng <tangwan.spare@gmail.com>
2024-01-10 15:59:37 +0800

    ⭐feat: 添加logo，暂时只有一个logo

--- src/stores/themeStore.ts
+++ src/stores/themeStore.ts
@@ -1,17 +1,16 @@
 import { defineStore } from "pinia";
 import { selectTheme } from "@/utils/theme";
+import photoLight from "@/assets/image/logo.png";
 import { ref } from "vue";
 export const usethemeStore = defineStore("themeStore", () => {
   const theme = ref("dark");
-  const darkLogo = "";
-  const lightLogo = "";
+  const darkLogo = photoLight;
+  const lightLogo = photoLight;
   const themeLogo = ref(darkLogo);
   function switchTheme(data: string) {
     selectTheme(data);
     theme.value = data;
-    data == "dark"
-      ? (themeLogo.value = darkLogo)
-      : (themeLogo.value = lightLogo);
+    data == "dark" ? (themeLogo.value = darkLogo) : (themeLogo.value = lightLogo);
   }
   return { theme, switchTheme, themeLogo };
 });
