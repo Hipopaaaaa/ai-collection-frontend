a5995797ccc946337274263aee48848cd39a9b7e
tangwancheng <tangwan.spare@gmail.com>
2024-01-12 09:51:51 +0800

    💡refactor：将部分远程图片更替为本地

--- src/utils/robots.ts
+++ src/utils/robots.ts
@@ -1,26 +1,26 @@
+import robot from "@/assets/image/robot.webp";
+import GPT4 from "@/assets/image/GPT4.0.webp";
 export const robots = [
   {
     roleId: "assistant",
     robotName: "Assistant",
-    avatar: "https://cdn.aiopen.cc/30031/1/robot.webp",
+    avatar: robot,
     operated: "Botalker",
     introduction:
-      "General-purpose assistant bot with strengths in programming-related tasks and non-English languages. Powered by gpt-3.5-turbo.",
+      "General-purpose assistant bot with strengths in programming-related tasks and non-English languages. Powered by gpt-3.5-turbo."
   },
   {
     roleId: "GPT-4.0",
     robotName: "GPT-4.0",
-    avatar: "https://cdn.aiopen.cc/30031/1/GPT4.0.webp",
+    avatar: GPT4,
     operated: "OpenAI",
     introduction:
-      "OpenAI's most powerful model. Stronger than ChatGPT in quantitative questions (math and physics), creative writing, and many other challenging tasks. Availability is currently limited.",
-  },
+      "OpenAI's most powerful model. Stronger than ChatGPT in quantitative questions (math and physics), creative writing, and many other challenging tasks. Availability is currently limited."
+  }
 ];
 
 // 根据roleId获取机器人信息
 export const getRobotInfo = (roleId: string) => {
   // 找不到默认assistant
-  return robots.find((robot) => robot.roleId === roleId)
-    ? robots.find((robot) => robot.roleId === roleId)
-    : robots[0];
+  return robots.find((robot) => robot.roleId === roleId) ? robots.find((robot) => robot.roleId === roleId) : robots[0];
 };
